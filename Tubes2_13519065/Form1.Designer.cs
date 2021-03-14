ad9f856d909fb63a475951779eb8443fbc2709ca
Muhammad Fahmi Alamsyah <49779495+tararahuuw@users.noreply.github.com>
2021-03-14 19:14:58 +0700

    Mapping and Design

--- Tubes2_13519065/Form1.Designer.cs
+++ Tubes2_13519065/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Tubes2_13519065
+﻿namespace Connect
 {
     partial class Form1
     {
@@ -29,64 +28,171 @@ namespace Tubes2_13519065
         /// </summary>
         private void InitializeComponent()
         {
+            this.button_LoadFile = new System.Windows.Forms.Button();
+            this.openFileGraph = new System.Windows.Forms.OpenFileDialog();
+            this.panel_DrawGraph = new System.Windows.Forms.Panel();
+            this.openFileQuery = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // button_LoadFile
+            // 
+            this.button_LoadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_LoadFile.Location = new System.Drawing.Point(302, 68);
+            this.button_LoadFile.Name = "button_LoadFile";
+            this.button_LoadFile.Size = new System.Drawing.Size(140, 43);
+            this.button_LoadFile.TabIndex = 0;
+            this.button_LoadFile.Text = "Browse";
+            this.button_LoadFile.UseVisualStyleBackColor = true;
+            this.button_LoadFile.Click += new System.EventHandler(this.button_LoadFile_Click);
+            // 
+            // openFileGraph
+            // 
+            this.openFileGraph.FileName = "openFileGraph";
+            // 
+            // panel_DrawGraph
+            // 
+            this.panel_DrawGraph.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel_DrawGraph.Location = new System.Drawing.Point(28, 175);
+            this.panel_DrawGraph.Name = "panel_DrawGraph";
+            this.panel_DrawGraph.Size = new System.Drawing.Size(784, 284);
+            this.panel_DrawGraph.TabIndex = 2;
+            // 
+            // openFileQuery
+            // 
+            this.openFileQuery.FileName = "openFileQuery";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "People You May Know";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Graph File          :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Algoritma            :";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(302, 133);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "DFS";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(397, 133);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "BFS";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(95, 489);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Choose Account          :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(93, 533);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(236, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Explore friends with     :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.DisplayMember = "A";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(337, 493);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AllowDrop = true;
+            this.comboBox2.DisplayMember = "A";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(337, 537);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 11;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(309, 352);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(98, 574);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 36);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(269, 329);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(46, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "BFS";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(415, 329);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(47, 17);
-            this.checkBox2.TabIndex = 2;
-            this.checkBox2.Text = "DFS";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(269, 138);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(193, 185);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(840, 749);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_LoadFile);
+            this.Controls.Add(this.panel_DrawGraph);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,10 +200,19 @@ namespace Tubes2_13519065
 
         #endregion
 
+        private System.Windows.Forms.Button button_LoadFile;
+        private System.Windows.Forms.OpenFileDialog openFileGraph;
+        private System.Windows.Forms.Panel panel_DrawGraph;
+        private System.Windows.Forms.OpenFileDialog openFileQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
-
--- Tubes2_13519065/Form1.cs
+++ Tubes2_13519065/Form1.cs
@@ -6,35 +6,113 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Threading;
 
-namespace Tubes2_13519065
+namespace Connect
 {
+
+
     public partial class Form1 : Form
     {
+        Microsoft.Msagl.Drawing.Graph graph; // The graph that MSAGL accepts
+        Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer(); // Graph viewer engine
+        Graph Graf;
+        int JumlahNode;
+        bool graphLoaded = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button_LoadFile_Click(object sender, EventArgs e)
         {
+            // Browse Document
+            openFileGraph.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileGraph.InitialDirectory = Directory.GetCurrentDirectory();
 
+            // Show file dialog
+            DialogResult result = openFileGraph.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                graph = new Microsoft.Msagl.Drawing.Graph("graph"); // Initialize new MSAGL graph                
+                // Read input file
+                using (StreamReader bacafile = new StreamReader(openFileGraph.OpenFile()))
+                {
+                    string baca = bacafile.ReadLine();
+                    if (baca == null || baca == "0")
+                    {
+                        MessageBox.Show("File Kosong");
+                    }
+                    else
+                    {
+                        graphLoaded = true;
+                        JumlahNode = Int32.Parse(baca);
+                        Graf = new Graph(JumlahNode);
+
+                        while (bacafile.Peek() >= 0)
+                        {
+                            baca = bacafile.ReadLine(); // Read file line by line
+                            string[] cur_line = baca.Split(' ');
+                            graph.AddEdge(cur_line[0], cur_line[1]);
+                            graph.AddEdge(cur_line[1], cur_line[0]);
+                        }
+
+                        try
+                        {
+                            DrawGraph();
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Input Text Mengandung Graf Siklik");
+                        }
+                    }
+                }
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void DrawGraph()
         {
-
+            // Bind graph to viewer engine
+            viewer.Graph = graph;
+            // Bind viewer engine to the panel
+            panel_DrawGraph.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel_DrawGraph.Controls.Add(viewer);
+            panel_DrawGraph.ResumeLayout();
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        //Graph declaration
+        class Graph
         {
+            private List<int>[] sisi;
+            private int[] depth;
+            private int simpul;
 
+            public Graph(int n)
+            {
+                simpul = n;
+                sisi = new List<int>[n + 1];
+                depth = new int[n + 1];
+                for (int i = 0; i <= n; i++)
+                {
+                    sisi[i] = new List<int>();
+                    depth[i] = 0;
+                }
+            }
+            ~Graph()
+            {
+                for (int i = 0; i <= simpul; i++)
+                {
+                    sisi[i] = null;
+                }
+                sisi = null;
+                depth = null;
+            }
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
