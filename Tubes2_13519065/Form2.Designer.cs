e8e531938a6ed42d24681232527d58a972a07d33
Muhammad Fahmi Alamsyah <49779495+tararahuuw@users.noreply.github.com>
2021-03-20 14:57:07 +0700

    sdftgsdf

--- Tubes2_13519065/Form2.Designer.cs
+++ Tubes2_13519065/Form2.Designer.cs
@@ -117,6 +117,7 @@ namespace Tubes2_13519065
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(316, 31);
             this.textBox2.TabIndex = 25;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // button2
             // 
